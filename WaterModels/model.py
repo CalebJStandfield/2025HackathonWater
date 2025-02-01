b98659d902aa5bc7fffd72aae41cbbecdc94740e
dhruvrachakonda <41206743+dhruvrachakonda@users.noreply.github.com>
2025-02-01 04:50:28 -0700

    model script

--- WaterModels/model.py
+++ WaterModels/model.py
@@ -5,49 +5,50 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV
 import pickle
 
-x_input = pd.DataFrame({
-    "Year": [2000],
-    "Month": [1],
-    "Rainfall": [2.17],
-    "Rainfall_3mo_avg": [2.17],
-    "Rainfall_6mo_avg": [2.17],
-    "Temperature": [35.1],
-    "Temperature_3mo_avg": [35.1],
-    "Temperature_6mo_avg": [35.1],
-    "GSL Levels": [4202.58],
-    "GSL_3mo_avg": [4202.58],
-    "GSL_6mo_avg": [4202.58],
-    "beaver_soil": [53.26],
-    "trial_soil": [75.72],
-    "parely_soil": [77.28],
-    "hayden_soil": [79.97]
-})
-
 def is_cat_0(x: pd.DataFrame):
-    loaded_model = pickle.load(open("0_100.pkl", 'rb'))
+    loaded_model = pickle.load(open("WaterModels\\0_100.pkl", 'rb'))
     result = loaded_model.predict(x)
 
-    if(x[0] == 0):
+    if(result[0] == 0):
         return False
     
     return True
 
 def is_cat_1(x: pd.DataFrame):
-    loaded_model = pickle.load(open("100_200.pkl", 'rb'))
+    loaded_model = pickle.load(open("WaterModels\\100_200.pkl", 'rb'))
     result = loaded_model.predict(x)
 
-    if(x[0] == 0):
+    if(result[0] == 0):
         return False
     
     return True
 
-def is_cat_1(x: pd.DataFrame):
-    loaded_model = pickle.load(open("100_200.pkl", 'rb'))
+def is_cat_2(x: pd.DataFrame):
+    loaded_model = pickle.load(open("WaterModels\\200_300.pkl", 'rb'))
     result = loaded_model.predict(x)
 
-    if(x[0] == 0):
+    if(result[0] == 0):
         return False
     
     return True
 
+def is_cat_3(x: pd.DataFrame):
+    loaded_model = pickle.load(open("WaterModels\\300_plus.pkl", 'rb'))
+    result = loaded_model.predict(x)
+
+    if(result[0] == 0):
+        return False
+    
+    return True
+
+
+def get_risk(x: pd.DataFrame):
+    if (is_cat_0(x)):
+        return "Little to no Risk"
+    if (is_cat_1(x)):
+        return "Moderate Risk"
+    elif (is_cat_2(x)):
+        return "High Risk"
+    elif (is_cat_3(x)):
+        return "Severe Risk"
 
