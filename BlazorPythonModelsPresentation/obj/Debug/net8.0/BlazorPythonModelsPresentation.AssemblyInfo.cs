9489c3128655830d999d770b21cadff4a9be0b76
CalebJStandfield <calstandfield@gmail.com>
2025-02-01 02:23:10 -0700

    fs

--- BlazorPythonModelsPresentation/obj/Debug/net8.0/BlazorPythonModelsPresentation.AssemblyInfo.cs
+++ BlazorPythonModelsPresentation/obj/Debug/net8.0/BlazorPythonModelsPresentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPythonModelsPresentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061a42df36f90017a98a4b0f21a52bfeca03f9a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad9309b154207f8fc38ecc08583abce8170cc1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPythonModelsPresentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPythonModelsPresentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
